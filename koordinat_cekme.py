a8ba27065703a20a375411148fc5bdd65e9aaa14
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-26 02:16:36 +0300

    Create koordinat_cekme.py

---
+++ koordinat_cekme.py
@@ -0,0 +1,12 @@
+# Gis verisinde python ile koordinat çekme
+
+layer = iface.activeLayer()
+""" Verinin kaydedilmesini istediğiniz yolu giriniz. 
+Örnek*: C:gis/xyz.txt
+"""
+output_file = open('Örnek*', 'w')
+for f in layer.getFeatures():
+    geom = f.geometry()
+    writeLine = str(f['name']) + ',' + str(geom.asPoint().y()) + ',' + str(geom.asPoint().x()) + ',' + str(geom.asPoint().x()) + '\n'
+    output_file.write(writeLine)
+output_file.close
