a94a3ce8d2764333fdcd321ace3205a0f873a84d
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-26 02:39:29 +0300

    Update tampon_duzenleme.py

--- tampon_duzenleme.py
+++ tampon_duzenleme.py
@@ -1,7 +1,10 @@
-url = "C:/Users/gnser/Desktop/python.zip"
+""" Verinin kaydedilmesini istediğiniz yolu giriniz. 
+Örnek* = C:gis/python.zip """
+url = "Örnek*"
 iface.addVectorLayer(url, "points", "ogr")
 layer = iface.activeLayer()
 result = processing.runAndLoadResults("native:buffer",
     {'INPUT':layer, 'DISTANCE':0.001, 'SEGMENTS':5, 'END_CAP_STYLE':0, 'JOIN_STYLE':0, 'MITER_LIMIT':2, 
     'DISSOLIVE':False, 'OUTPUT':'memory:buffered'})
     
+ 
